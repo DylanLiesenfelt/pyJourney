485afaac351b4792f9305d3d3ee91591c1e4d00d
DylanLiesenfelt <dylanjames.code@gmail.com>
2023-12-08 16:37:30 -0500

    Create pyJourney.py

---
+++ pyJourney.py
@@ -0,0 +1,20 @@
+def displayMenu():
+    print('1.Stats')
+    print('2.Inventory')
+    menu = input('Please Enter Menu Option: ')
+    return menu
+
+letPlay = 1
+while letPlay == 1:
+    playerStrength
+    playerToughness
+    playerDexterity
+    playerPerception  
+    
+
+
+    menu = displayMenu()
+    if menu == 1:
+
+    elif menu == 2:
+    
