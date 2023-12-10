916d7dccf7ec970a9e9e387a1f40f282643d632d
DylanLiesenfelt <dylanjames.code@gmail.com>
2023-12-09 22:13:48 -0500

    Update pyJourney.py

--- pyJourney.py
+++ pyJourney.py
@@ -6,11 +6,29 @@ def displayMenu():
 
 letPlay = 1
 while letPlay == 1:
-    playerStrength
-    playerToughness
-    playerDexterity
-    playerPerception  
+
+    playerStrength = 1
+    playerToughness = 1
+    playerDexterity = 1
+    playerPerception = 1 
     
+    playerBlood = 100
+    playerHead = 100
+    playerTorso = 100
+    playerLeftArm = 100
+    playerRightArm = 100
+    playerLeftLeg = 100
+    playerRightLeg = 100
+
+    armourHead = 0
+    armourTorso = 0
+    armourLeftArm = 0
+    armourRightArm = 0
+    armourLeftLeg = 0
+    armourRightLeg = 0 
+
+    maxWeight = (19 + (1 * playerStrength))
+
 
 
     menu = displayMenu()
